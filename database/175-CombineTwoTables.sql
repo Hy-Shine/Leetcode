90c693c51c5078aaefef9e84cd94847de6a764b0
ley <shiyaleinl@gmail.com>
2020-10-07 22:56:27 +0800

    added 175-CombineTwoTables.sql and 595-BigCountries.sql solutions

---
+++ database/175-CombineTwoTables.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+SELECT a.FirstName, a.LastName, b.City, b.State
+FROM 
+    Person a 
+    LEFT JOIN Address b ON a.PersonId = b.PersonId
+
+-- Write your MySQL query statement below
+SELECT a.FirstName, a.LastName, b.City, b.State
+FROM 
+    Person a 
+    LEFT JOIN Address b ON a.PersonId = b.PersonId
