51f5fdd6381303c79affe7384cf3826e62b26f97
ley <shiyaleinl@gmail.com>
2020-11-03 00:30:37 +0800

    added 185-DepartmentTopThreeSalaries.sql

---
+++ database/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select Department,Employee,salary from (
+select t2.name Department,t.name Employee,t.salary,
+dense_rank() OVER (PARTITION BY t.departmentid ORDER BY t.salary desc) pxh
+	from Employee t,Department t2 where t.departmentid=t2.id
+) where pxh <=3
