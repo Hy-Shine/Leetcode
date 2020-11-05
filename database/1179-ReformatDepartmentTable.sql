dc6913ce2668beed81bf953efd2a7c549d7fb777
ley <shiyaleinl@gmail.com>
2020-11-05 23:18:41 +0800

    added 1179-ReformatDepartmentTable.sql and 180-ConsecutiveNumbers.sql

---
+++ database/1179-ReformatDepartmentTable.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select * from department
+	pivot(max(revenue) for month in 
+		('Jan' Jan_Revenue,'Feb' Feb_Revenue,'Mar' Mar_Revenue,
+			'Apr' Apr_Revenue,'May' May_Revenue,'Jun' Jun_Revenue,
+			'Jul' Jul_Revenue,'Aug' Aug_Revenue,'Sep'	Sep_Revenue,
+			'Oct' Oct_Revenue,'Nov' Nov_Revenue,'Dec'	Dec_Revenue))
+order by id;
---
+++ database/180-ConsecutiveNumbers.sql
@@ -0,0 +1,19 @@
+/* Write your PL/SQL query statement below */
+SELECT DISTINCT t.num ConsecutiveNums 
+	FROM logs t,logs t2,logs t3 
+WHERE t.id=(t2.id-1) AND t.num=t2.num 
+	AND t2.id=(t3.id-1) AND t2.num=t3.num;
+
+SELECT DISTINCT t.num ConsecutiveNums 
+	FROM logs t 
+	LEFT JOIN logs t2 ON t.id=(t2.id-1) AND t.NUM=t2.num
+	LEFT JOIN logs t3 ON t2.id=(t3.id-1) AND t2.num=t3.num
+WHERE t3.id IS NOT NULL;
+
+-- 可以获得指定连续次数的数字
+SELECT DISTINCT num ConsecutiveNums 
+FROM ( 
+	SELECT a.num,a.id-a.order1 FROM (
+		SELECT t.*,ROW_NUMBER() OVER(PARTITION BY num ORDER BY id) order1
+			FROM logs t) a GROUP BY num,(a.id-a.order1) HAVING COUNT(1)>2 
+);
