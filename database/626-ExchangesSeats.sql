c74dc4cc29bc453cdd129c358a1190de44974a7d
ley <shiyaleinl@gmail.com>
2021-06-06 20:53:54 +0800

    626-ExchangesSeats.sql

---
+++ database/626-ExchangesSeats.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+CASE WHEN mod(id,2)=0 THEN id-1 
+	WHEN id=(SELECT count(id) FROM seat) AND MOD(id,2)=1 THEN id 
+	WHEN MOD(id,2)=1 THEN id+1 
+END id,
+student
+FROM seat ORDER BY id;
